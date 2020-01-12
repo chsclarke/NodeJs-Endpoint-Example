59ea37be8efcc1cba323d72fb8b0bcb53f7961a4
Chase Clarke <chaseclarke@Chase.local>
2020-01-12 13:26:33 -0800

    Added code headers

--- server.js
+++ server.js
@@ -1,27 +1,32 @@
-var http = require('http');
-var url = require('url');
-var fs = require('fs');
+// =====================================================
+//  Created on 1/11/2020
+//  Copyright © 2020 Chase Clarke. All rights reserved.
+// =====================================================
+
+var http = require("http");
+var url = require("url");
+var fs = require("fs");
 
 var WEBPORT = 8080;
 
-console.log('>> Initializing server on port: ' + WEBPORT);
+console.log(">> Initializing server on port: " + WEBPORT);
 
 // function below creates server object and begins listening
 http.createServer(function (req, res) {
 
-  // 'parse' seperates the url path and url args into a usable JSON object
+  // "parse" seperates the url path and url args into a usable JSON object
   var q = url.parse(req.url, true);
   // filename used to return html webpages
   var filename = "static/" + q.pathname;
 
 
-  // '/weather' endpoint
+  // "/weather" endpoint
   // example: http://localhost:3000/weather?ID=1
   if (q.pathname == "/weather") {
-    res.setHeader('Content-Type', 'application/json');    
+    res.setHeader("Content-Type", "application/json");    
 
     if (q.query.ID) {
-      console.log('>> Retrieiving data @ ID: ' + q.query.ID);
+      console.log(">> Retrieiving data @ ID: " + q.query.ID);
       var temp = getTemp(q.query.ID);
       return res.end(temp);
     }
@@ -30,19 +35,19 @@ http.createServer(function (req, res) {
       return res.end("{ \"ERROR\" : \"you must enter a tempId\"}");
   }
 
-  // if not '/weather', look for html page within /static path to return to web browser
+  // if not "/weather", look for html page within /static path to return to web browser
   // most useful for redirects to a 404 page.
   else{
     fs.readFile(filename, function(err, data) {
       if (err) {
         fs.readFile("static/404.html", function(err, data) {
-          res.writeHead(200, {'Content-Type': 'text/html'});
+          res.writeHead(200, {"Content-Type": "text/html"});
           res.write(data);
           return res.end();
         });
       }
       else {
-        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.writeHead(200, {"Content-Type": "text/html"});
         res.write(data);
         return res.end();
       }
